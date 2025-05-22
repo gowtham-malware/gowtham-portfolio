7689770a8a7ffc0378dbaa9af14b705b057c9c51
gowtham-malware <55401731+gowtham-malware@users.noreply.github.com>
2025-05-22 12:12:26 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Portfolio from "./components/Portfolio";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Portfolio />);
